27a4eca9d95e32cd4a2aebb04171ef3aa16f221c
Gantulga <gangstarmn@gmail.com>
2024-04-24 20:20:02 +0800

    extion added

---
+++ ios/MYWidget/MYWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  MYWidgetBundle.swift
+//  MYWidget
+//
+//  Created by Gantulga ts on 2024.04.24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MYWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        MYWidget()
+    }
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import app_links
+import mobile_scanner
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
+  MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
 }
